3484a5bf1f09765b11a291908c113bcdcfc02ebf
Samuel Dion <samuelqwe@icloud.com>
2024-01-29 12:20:19 -0500

    add Alire formula

---
+++ Formula/alire.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Alire < Formula
+  desc "Command-line tool from the Alire project and supporting library"
+  homepage "https://alire.ada.dev"
+  url "https://github.com/alire-project/alire/releases/download/v1.2.2/alr-1.2.2-bin-x86_64-macos.zip"
+  sha256 "7e568dfa8d9a785ef614086a1ab64d1a6853581c574ee661cffc8b558fd0bfaa"
+  license "GPL-3.0-only"
+
+  def install
+    bin.install "bin/alr"
+  end
+
+  test do
+    system "false"
+  end
+end
