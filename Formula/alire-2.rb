ebe943a547dec61a856aefc9faeb612920bc5345
Samuel Dion <samuelqwe@icloud.com>
2024-03-04 02:00:04 -0500

    Add Alire 2 RC1

---
+++ Formula/alire-2.rb
@@ -0,0 +1,15 @@
+class Alire < Formula
+  desc "Command-line tool from the Alire project and supporting library"
+  homepage "https://alire.ada.dev"
+  url "https://github.com/alire-project/alire/releases/download/v2.0.0-rc1/alr-2.0.0-rc1-bin-x86_64-macos.zip"
+  sha256 "133b60b306334cfc1a56fb564917a62743219fa650dc50552a60b7fce4ec52f6"
+  license "GPL-3.0-only"
+
+  def install
+    bin.install "bin/alr"
+  end
+
+  test do
+    system "false"
+  end
+end
