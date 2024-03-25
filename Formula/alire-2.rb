e72a92095791f41c4f808e0f6980679773a7e7e7
Samuel Dion <samuelqwe@icloud.com>
2024-03-25 08:57:27 -0400

    Update Alire 2 formula to version 2.0.1

--- Formula/alire-2.rb
+++ Formula/alire-2.rb
@@ -1,8 +1,8 @@
 class Alire2 < Formula
   desc "Command-line tool from the Alire project and supporting library"
   homepage "https://alire.ada.dev"
-  url "https://github.com/alire-project/alire/releases/download/v2.0.0/alr-2.0.0-bin-x86_64-macos.zip"
-  sha256 "029b2f90933408ea95a353c392170e47305b2142498db0d322d814ccd3f0407f"
+  url "https://github.com/alire-project/alire/releases/download/v2.0.1/alr-2.0.1-bin-x86_64-macos.zip"
+  sha256 "74223012ded5bf8057a7dd60856665f22e85a937a53c37678d212c60122de0c2"
   license "GPL-3.0-only"
 
   def install
